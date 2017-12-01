7b8d26ad77f7e0e6230aa49de0df30529aceb58b
MinaNemr <minanemr7@gmail.com>
2017-12-01 20:06:11 +0200

    first depy

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -22,7 +22,7 @@ angular.module('yapp')
         var day = dateNow.getDate();
         console.log(dateNow);
         var dateNowString = "" + day + month + year
-        if (dateNow>=timeMin && dateNow <= timeMax && $scope.user.prayers.indexOf(dateNowString) == -1) {
+        if (dateNow>=timeMin && dateNow <= timeMax ) {
              $scope.displayBox = true;
             }
         $scope.blaz = function (){
